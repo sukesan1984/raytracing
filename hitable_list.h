30f9b42569ab9c613a07d2dc3ad8b7d2c2a857d1
Sukesan1984 <takamikousuke@gmail.com>
2020-11-07 19:05:17 +0900

    Modify hitable_list to use make_shared and shared_ptr

--- hitable_list.h
+++ hitable_list.h
@@ -2,27 +2,32 @@
 #define HITABLELISTH
 
 #include "hitable.h"
+#include <memory>
+#include <vector>
+
+using std::shared_ptr;
+using std::make_shared;
 
 class hitable_list: public hitable {
-	public:
+public:
 		hitable_list() {}
-		hitable_list(hitable **l, int n) {
-			list = l;
-			list_size = n;
-		}
+		hitable_list(shared_ptr<hitable> object)  { add(object); }
+		void clear() { objects.clear(); }
+		void add(shared_ptr<hitable> object) { objects.push_back(object); }
 		virtual bool hit(const ray& r, float tmin, float tmax,
 				hit_record& rec) const;
-		hitable **list;
-		int list_size;
+public:
+	std::vector<shared_ptr<hitable>> objects;
 };
 
 bool hitable_list::hit(const ray& r, float t_min, float t_max,
 		hit_record& rec) const {
 	hit_record temp_rec;
 	bool hit_anything = false;
-	double closest_so_far = t_max;
-	for (int i = 0; i < list_size; i++) {
-		if (list[i]->hit(r, t_min, closest_so_far, temp_rec)) {
+	auto closest_so_far = t_max;
+
+	for (const auto& object : objects) {
+		if (object->hit(r, t_min, closest_so_far, temp_rec)) {
 			hit_anything = true;
 			closest_so_far = temp_rec.t;
 			rec = temp_rec;
@@ -30,4 +35,5 @@ bool hitable_list::hit(const ray& r, float t_min, float t_max,
 	}
 	return hit_anything;
 }
+
 #endif
--- main.cc
+++ main.cc
@@ -6,9 +6,9 @@
 #include "hitable_list.h"
 #include "color.h"
 
-vec3 ray_color(const ray& r, hitable *world, int depth) {
+vec3 ray_color(const ray& r, hitable_list world, int depth) {
     hit_record rec;
-    if (world->hit(r, 0.001, MAXFLOAT, rec)) {
+    if (world.hit(r, 0.001, MAXFLOAT, rec)) {
         ray scattered;
         vec3 attenuation;
         if (depth < 50 && rec.mat_ptr->scatter(r, rec, attenuation, scattered)) {
@@ -24,13 +24,13 @@ vec3 ray_color(const ray& r, hitable *world, int depth) {
     }
 }
 
-hitable *random_scene() {
+hitable_list random_scene() {
     int n = 500;
     int num_a = 3;
     int num_b = 3;
-    hitable **list = new hitable*[n+1];
-    list[0] = new sphere(vec3(0, -1000, 0), 1000, new lambertian(
-                vec3(0.5, 0.5, 0.5)));
+    hitable_list world;
+    world.add(make_shared<sphere>(vec3(0, -1000, 0), 1000, new lambertian(
+                vec3(0.5, 0.5, 0.5))));
     int i = 1;
     for (int a = -num_a; a < num_a; a++) {
         for (int b = -num_b; b < num_b; b++) {
@@ -38,12 +38,12 @@ hitable *random_scene() {
             vec3 center(a + 0.9 * drand48(), 0.2, b + 0.9 * drand48());
             if ((center - vec3(4, 0.2, 0)).length() > 0.9) {
                 if (choose_mat < 0.8) {
-                    list[i++] = new sphere(center, 0.2, new lambertian(
+                    world.add(make_shared<sphere>(center, 0.2, new lambertian(
                                 vec3(drand48() * drand48(), drand48() * drand48(),
-                                    drand48() * drand48())));
+                                    drand48() * drand48()))));
                 }
                 else if (choose_mat < 0.95) {
-                    list[i++] = new sphere(
+                    world.add(make_shared<sphere>(
                             center,
                             0.2,
                             new metal(
@@ -52,20 +52,20 @@ hitable *random_scene() {
                                     0.5 * (1 + drand48()),
                                     0.5 * (1 + drand48())
                                 ),
-                            0.5 * drand48()));
+                            0.5 * drand48())));
                 }
                 else {
-                    list[i++] = new sphere(center, 0.2, new dielectric(1.5));
+                    world.add(make_shared<sphere>(center, 0.2, new dielectric(1.5)));
                 }
             }
         }
     }
-    list[i++] = new sphere(vec3(0, 1, 0), 1.0, new dielectric(1.5));
-    list[i++] = new sphere(vec3(-4, 1, 0), 1.0,
-            new lambertian(vec3(0.4, 0.2, 0.1)));
-    list[i++] = new sphere(vec3(4, 1, 0), 1.0,
-            new metal(vec3(0.7, 0.6, 0.5), 0.0));
-    return new hitable_list(list, i);
+    world.add(make_shared<sphere>(vec3(0, 1, 0), 1.0, new dielectric(1.5)));
+    world.add(make_shared<sphere>(vec3(-4, 1, 0), 1.0,
+            new lambertian(vec3(0.4, 0.2, 0.1))));
+    world.add(make_shared<sphere>(vec3(4, 1, 0), 1.0,
+            new metal(vec3(0.7, 0.6, 0.5), 0.0)));
+    return world;
 }
 
 int main() {
@@ -95,7 +95,7 @@ int main() {
     //        new dielectric(1.5));
     //hitable *world = new hitable_list(list, 4);
     //hitable *world = new hitable_list(list, 2);
-    hitable *world = random_scene();
+    auto world = random_scene();
     for (int j = ny - 1; j >= 0; j--) {
         std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < nx; i++) {
